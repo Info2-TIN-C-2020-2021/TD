c3bb8cdef36aa035e26531bab6da12db11ac4524
pierrebressy <pierre.bressy@heig-vd.ch>
2021-03-29 16:59:50 +0200

    Add files via upload

---
+++ TD20210329.c
@@ -0,0 +1,28 @@
+/**
+  \file      TD20210329.c
+  \brief     pointers.... :)
+  \author    Pierre BRESSY
+  \version   1.0
+  \date      2021-03-29 14:55:00
+  \details
+
+**/
+
+#include <stdio.h>  // standard library for inputs and ouputs
+#include <stdint.h>
+
+int main(int argc, const char *argv[])
+{
+  int32_t i = 0;
+  int16_t j = 42;
+
+  printf("adress of i: %p\n", &i);
+  printf("adress of j: %p\n", &j);
+  return 0;
+}
+
+
+
+
+
+
